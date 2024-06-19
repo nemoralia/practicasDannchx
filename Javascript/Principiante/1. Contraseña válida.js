84da1b2cad117988099e5a9c724660a57a09db4d
Nemoralia <f3nriblack@gmail.com>
2024-06-18 21:00:40 -0500

    Se realiza la primera carpeta con el ejercicio 1 y el listado de ejercicios a realizar

---
+++ Javascript/Principiante/1. Contraseña válida.js
@@ -0,0 +1,10 @@
+/* 1. Contraseña válida
+Escribir una función llamada contrasenaValida que reciba un string y retorne true si 
+el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9". De lo contrario debe retornar false.
+
+// código de prueba
+console.log(contrasenaValida("2Fj(jjbFsuj")) // true
+console.log(contrasenaValida("eoZiugBf&g9")) // true
+console.log(contrasenaValida("hola")) // false
+console.log(contrasenaValuda("")) // false */
+
